2553744a3001d40b93d161f4a1e3a05afd42a01e
Sourav Saha <shanto.sourav07@gmail.com>
2022-10-11 22:14:07 +0600

    Blogs Section Done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            Blogs
+        <div className='mt-48 w-3/4 mx-auto'>
+            <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    What is the purpose of React Router?
+                </div>
+                <div className="collapse-content">
+                    <p>
+                        React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+                    </p>
+                </div>
+            </div>
+
+            <br />
+
+            <div tabIndex={1} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    How Does Context API works?
+                </div>
+                <div className="collapse-content">
+                    <p>
+                        The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+                    </p>
+                </div>
+            </div>
+
+            <br />
+
+            <div tabIndex={2} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div className="collapse-title text-xl font-medium">
+                    What is 'useHref()' hook?
+                </div>
+                <div className="collapse-content">
+                    <p>
+                        The useHref hook returns a URL that may be used to link to the given to location, even outside of React Router.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Layouts/Layouts.js
+++ src/Layouts/Layouts.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavBar from '../Components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Components/Footer/Footer';
 
 const Layouts = () => {
     return (
         <div>
             <NavBar></NavBar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
         </div>
     );
 };
