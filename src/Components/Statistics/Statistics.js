ab7bd99a73046ae3250dc5e3da4cb8c3f843592f
Sourav Saha <shanto.sourav07@gmail.com>
2022-10-11 21:37:52 +0600

    Statistics Chart Done

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const Statistics = () => {
+export default function Statistics() {
+    const quizLoader = useLoaderData();
+    const quizCategories = quizLoader.data;
+
     return (
-        <div>
-            Statistics
-        </div>
+        <ResponsiveContainer width="95%" height={400}>
+            <BarChart
+                className="barChart mt-32"
+                width={600}
+                height={300}
+                data={quizCategories}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" barSize={4} fill="#05386b" />
+            </BarChart>
+        </ResponsiveContainer>
     );
-};
-
-export default Statistics;
+}
